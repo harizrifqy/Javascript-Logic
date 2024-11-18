a24b3eef7c4145a241608d4078f77311d67f64e9
anaktjkt <anaktjkt@gmail.com>
2024-11-18 19:28:37 +0700

    belajar alert, promt, dan else if statement

---
+++ main.js
@@ -0,0 +1,63 @@
+// alert(1 + 1);
+// console.log('hello world!');
+
+
+// console.log('Sebelum pembanding')
+// if(1 + 1 === 2) {  /* akan dijalankan ketika bernilai true atau benar*/
+//     console.log('Perhitungannya Betul')
+// }
+// console.log('Setelah pembanding')
+
+
+// IF STATEMENT 
+
+// let angka = Math.random(); variable angka akan menampilkan angka secara random ketika direfresh
+// console.log(angka);
+
+// if(angka >= 0.5) {
+//     console.log('angka lebih besar dari 0.5')
+// }
+
+// if(angka <= 0.5) {
+//     console.log('angka lebih kecil dari 0.5')
+// }
+
+// ELSE IF STATMENT
+
+// const iniHari = 'Senin';
+
+// if(iniHari === 'Jumat') {
+//     console.log('Sebentar lagi libur kawan..')
+// } else if (iniHari === 'Senin') {
+//     console.log('Tetap semangat kawan....')
+// }
+
+
+// const nilai = '50';
+
+// if (nilai < 50 ) {
+//     console.log('E');
+// } else if (nilai < 60 ) {
+//     console.log('D');
+// } else if (nilai < 70 ) {
+//     console.log('C');
+// } else if (nilai < 80 ) {
+//     console.log('B');
+// } else if (nilai < 100 ) {
+//     console.log('A');
+// } 
+
+
+// const iniHari = prompt('Isi hari sesuai dengan hari favoritmu!!').toLowerCase();
+
+// if(iniHari === 'sabtu') {
+//     console.log('Healingg dulu masss..')
+// } else if (iniHari === 'senin') {
+//     console.log('Tetap semangat ya...')
+// } else {
+//     console.log('Santai dulu kawan..')
+// }
+
+
+
+
